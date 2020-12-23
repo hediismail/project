eb2b70b859e620583d127a62560b3faa798d8320
gaith1990 <gaith.mhathbi2@gmail.com>
2020-12-23 13:37:13 +0100

    disigneprofile

--- client/src/pages/profil.js
+++ client/src/pages/profil.js
@@ -24,100 +24,210 @@ const Profil = (props) => {
 console.log(pro)
 
     return (
-        <div> 
-
-
-             <div className="page-header header-filter" data-parallax="true" style={{backgroundImage: 'url("../assets/img/city-profile.jpg")'}} />
-        <div className="main main-raised">
-          <div className="profile-content">
-            <div className="container">
-              <div className="row">
-                <div className="col-md-6 ml-auto mr-auto">
-                  <div className="profile">
-                    <div className="avatar">
-                      <img src="../assets/img/faces/christian.jpg" alt="Circle Image" className="img-raised rounded-circle img-fluid" />
-                    </div>
-                    <div className="name">
-                      <h3 className="title">{pro.profileName}</h3>
-                      <h6>{pro.contact}</h6>
-                      <a href="#pablo" className="btn btn-just-icon btn-link btn-dribbble"><i className="fa fa-dribbble" /></a>
-                      <a href="#pablo" className="btn btn-just-icon btn-link btn-twitter"><i className="fa fa-twitter" /></a>
-                      <a href="#pablo" className="btn btn-just-icon btn-link btn-pinterest"><i className="fa fa-pinterest" /></a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="description text-center">
-                <p>{pro.about}</p>
-              </div>
-              <div className="row">
-                <div className="col-md-6 ml-auto mr-auto">
-                  <div className="profile-tabs">
-                    <ul className="nav nav-pills nav-pills-icons justify-content-center" role="tablist">
-                      <li className="nav-item">
-                        <a className="nav-link active" href="#studio" role="tab" data-toggle="tab">
-                          <i className="material-icons">camera</i> Studio
-                        </a>
-                      </li>
-                      <li className="nav-item">
-                        <a className="nav-link" href="#works" role="tab" data-toggle="tab">
-                          <i className="material-icons">palette</i> Work
-                        </a>
-                      </li>
-                      <li className="nav-item">
-                        <a className="nav-link" href="#favorite" role="tab" data-toggle="tab">
-                          <i className="material-icons">favorite</i> commentaires
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-              <div className="tab-content tab-space">
-                <div className="tab-pane active text-center gallery" id="studio">
-                  <div className="row">
-                    <div className="col-md-3 ml-auto">
-                      <img src="../assets/img/examples/studio-1.jpg" className="rounded" />
-                      <img src="../assets/img/examples/studio-2.jpg" className="rounded" />
-                    </div>
-                    <div className="col-md-3 mr-auto">
-                      <img src="../assets/img/examples/studio-5.jpg" className="rounded" />
-                      <img src="../assets/img/examples/studio-4.jpg" className="rounded" />
-                    </div>
-                  </div>
-                </div>
-                <div className="tab-pane text-center gallery" id="works">
-                  <div className="row">
-                    <div className="col-md-3 ml-auto">
-                      <img src="../assets/img/examples/olu-eletu.jpg" className="rounded" />
-                      <img src="../assets/img/examples/clem-onojeghuo.jpg" className="rounded" />
-                      <img src="../assets/img/examples/cynthia-del-rio.jpg" className="rounded" />
-                    </div>
-                    <div className="col-md-3 mr-auto">
-                      <img src="../assets/img/examples/mariya-georgieva.jpg" className="rounded" />
-                      <img src="../assets/img/examples/clem-onojegaw.jpg" className="rounded" />
-                    </div>
-                  </div>
-                </div>
-                <div className="tab-pane text-center gallery" id="favorite">
-                  <div className="row">
-                    <div className="col-md-3 ml-auto">
-                      <img src="../assets/img/examples/mariya-georgieva.jpg" className="rounded" />
-                      <img src="../assets/img/examples/studio-3.jpg" className="rounded" />
-                    </div>
-                    <div className="col-md-3 mr-auto">
-                      <img src="../assets/img/examples/clem-onojeghuo.jpg" className="rounded" />
-                      <img src="../assets/img/examples/olu-eletu.jpg" className="rounded" />
-                      <img src="../assets/img/examples/studio-1.jpg" className="rounded" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+       <div>
+   {/* Navbar on small screens */}
+   <div id="navDemo" className="w3-bar-block w3-theme-d2 w3-hide w3-hide-large w3-hide-medium w3-large">
+     <a href="#" className="w3-bar-item w3-button w3-padding-large">Link 1</a>
+     <a href="#" className="w3-bar-item w3-button w3-padding-large">Link 2</a>
+     <a href="#" className="w3-bar-item w3-button w3-padding-large">Link 3</a>
+     <a href="#" className="w3-bar-item w3-button w3-padding-large">My Profile</a>
+   </div>
+   {/* Page Container */}
+   <div className="w3-container w3-content" style={{maxWidth: '1400px', marginTop: '80px'}}>    
+     {/* The Grid */}
+     <div className="w3-row">
+       {/* Left Column */}
+       <div className="w3-col m3">
+         {/* Profile */}
+         <div className="w3-card w3-round w3-white">
+           <div className="w3-container">
+             <h4 className="w3-center">{pro.profileName}</h4>
+             <p className="w3-center"><img src="https://lh3.googleusercontent.com/proxy/XgPnzKo35twy7Gof21AWTr7RqLN1VXp_4VaDuHKePZWIi7CO7PRvsxgDZQ60Obd-NdR57c9hlOuFz3s-fhx5fgtyNcPJgIgLDmAw" className="w3-circle" style={{height: '106px', width: '106px'}} alt="Avatar" /></p>
+             <hr />
+             <p><i className="fa fa-pencil fa-fw w3-margin-right w3-text-theme" /> {pro.catégorie}</p>
+             <p><i className="fa fa-phone fa-fw w3-margin-right w3-text-theme" /> {pro.contact}</p>
+             <p><i className="fa fa-envelope-open fa-fw w3-margin-right w3-text-theme" /> April 1, 1988</p>
+           </div>
+         </div>
+         <br />
+         {/* Accordion */}
+         <div className="w3-card w3-round">
+           <div className="w3-white">
              
-        </div>
+             <button onclick="myFunction('Demo3')" className="w3-button w3-block w3-theme-l1 w3-left-align"><i className="fa fa-users fa-fw w3-margin-right" /> My Photos</button>
+             <div id="Demo3" className="w3-hide w3-container w3-show">
+               <div className="w3-row-padding">
+                 <br />
+                 <div className="w3-half">
+                   <img src="/w3images/lights.jpg" style={{width: '100%'}} className="w3-margin-bottom" />
+                 </div>
+                 <div className="w3-half">
+                   <img src="/w3images/nature.jpg" style={{width: '100%'}} className="w3-margin-bottom" />
+                 </div>
+                 <div className="w3-half">
+                   <img src="/w3images/mountains.jpg" style={{width: '100%'}} className="w3-margin-bottom" />
+                 </div>
+                 <div className="w3-half">
+                   <img src="/w3images/forest.jpg" style={{width: '100%'}} className="w3-margin-bottom" />
+                 </div>
+                 <div className="w3-half">
+                   <img src="/w3images/nature.jpg" style={{width: '100%'}} className="w3-margin-bottom" />
+                 </div>
+                 <div className="w3-half">
+                   <img src="/w3images/snow.jpg" style={{width: '100%'}} className="w3-margin-bottom" />
+                 </div>
+               </div>
+             </div>
+             <button onclick="myFunction('Demo3')" className="w3-button w3-block w3-theme-l1 w3-left-align"><i className="fa fa-users fa-fw w3-margin-right" /> My Video</button>
+             <div id="Demo3" className="w3-hide w3-container w3-show">
+               <div className="w3-row-padding">
+                 <br />
+                 <div className="w3-half">
+                   <img src="/w3images/lights.jpg" style={{width: '100%'}} className="w3-margin-bottom" />
+                 </div>
+                 <div className="w3-half">
+                   <img src="/w3images/nature.jpg" style={{width: '100%'}} className="w3-margin-bottom" />
+                 </div>
+                 <div className="w3-half">
+                   <img src="/w3images/mountains.jpg" style={{width: '100%'}} className="w3-margin-bottom" />
+                 </div>
+                 <div className="w3-half">
+                   <img src="/w3images/forest.jpg" style={{width: '100%'}} className="w3-margin-bottom" />
+                 </div>
+                 <div className="w3-half">
+                   <img src="/w3images/nature.jpg" style={{width: '100%'}} className="w3-margin-bottom" />
+                 </div>
+                 <div className="w3-half">
+                   <img src="/w3images/snow.jpg" style={{width: '100%'}} className="w3-margin-bottom" />
+                 </div>
+               </div>
+             </div>
+           </div>      
+         </div>
+         <br />
+         {/* Interests */} 
+         <div className="w3-card w3-round w3-white w3-hide-small">
+           <div className="w3-container">
+             <p>About Us</p>
+             <p>
+               
+               {pro.about}
+             </p>
+           </div>
+         </div>
+         <br />
+         {/* Alert Box */}
+         <div className="w3-container w3-display-container w3-round w3-theme-l4 w3-border w3-theme-border w3-margin-bottom w3-hide-small">
+           <span onclick="this.parentElement.style.display='none'" className="w3-button w3-theme-l3 w3-display-topright">
+             <i className="fa fa-remove" />
+           </span>
+           <p><strong>Hey!</strong></p>
+           <p>People are looking at your profile. Find out who.</p>
+         </div>
+         {/* End Left Column */}
+       </div>
+       {/* Middle Column */}
+       <div className="w3-col m7">
+         <div className="w3-row-padding">
+           <div className="w3-col m12">
+             <div className="w3-card w3-round w3-white">
+               <div className="w3-container w3-padding">
+                 <h6 className="w3-opacity">Social Media template by w3.css</h6>
+                 <p contentEditable="true" className="w3-border w3-padding">Status: Feeling Blue</p>
+                 <button type="button" className="w3-button w3-theme"><i className="fa fa-pencil" /> &nbsp;Post</button> 
+               </div>
+             </div>
+           </div>
+         </div>
+         <div className="w3-container w3-card w3-white w3-round w3-margin"><br />
+           <img src="https://lh3.googleusercontent.com/proxy/XgPnzKo35twy7Gof21AWTr7RqLN1VXp_4VaDuHKePZWIi7CO7PRvsxgDZQ60Obd-NdR57c9hlOuFz3s-fhx5fgtyNcPJgIgLDmAw" alt="Avatar" className="w3-left w3-circle w3-margin-right" style={{width: '60px'}} />
+           <span className="w3-right w3-opacity">1 min</span>
+           <h4>John Doe</h4><br />
+           <hr className="w3-clear" />
+           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+           <div className="w3-row-padding" style={{margin: '0 -16px'}}>
+             <div className="w3-half">
+               <img src="/w3images/lights.jpg" style={{width: '100%'}} alt="Northern Lights" className="w3-margin-bottom" />
+             </div>
+             <div className="w3-half">
+               <img src="/w3images/nature.jpg" style={{width: '100%'}} alt="Nature" className="w3-margin-bottom" />
+             </div>
+           </div>
+           <button type="button" className="w3-button w3-theme-d1 w3-margin-bottom"><i className="fa fa-thumbs-up" /> &nbsp;Like</button> 
+           <button type="button" className="w3-button w3-theme-d2 w3-margin-bottom"><i className="fa fa-comment" /> &nbsp;Comment</button> 
+         </div>
+         <div className="w3-container w3-card w3-white w3-round w3-margin"><br />
+           <img src="/w3images/avatar5.png" alt="Avatar" className="w3-left w3-circle w3-margin-right" style={{width: '60px'}} />
+           <span className="w3-right w3-opacity">16 min</span>
+           <h4>Jane Doe</h4><br />
+           <hr className="w3-clear" />
+           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+           <button type="button" className="w3-button w3-theme-d1 w3-margin-bottom"><i className="fa fa-thumbs-up" /> &nbsp;Like</button> 
+           <button type="button" className="w3-button w3-theme-d2 w3-margin-bottom"><i className="fa fa-comment" /> &nbsp;Comment</button> 
+         </div>  
+         <div className="w3-container w3-card w3-white w3-round w3-margin"><br />
+           <img src="/w3images/avatar6.png" alt="Avatar" className="w3-left w3-circle w3-margin-right" style={{width: '60px'}} />
+           <span className="w3-right w3-opacity">32 min</span>
+           <h4>Angie Jane</h4><br />
+           <hr className="w3-clear" />
+           <p>Have you seen this?</p>
+           <img src="/w3images/nature.jpg" style={{width: '100%'}} className="w3-margin-bottom" />
+           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+           <button type="button" className="w3-button w3-theme-d1 w3-margin-bottom"><i className="fa fa-thumbs-up" /> &nbsp;Like</button> 
+           <button type="button" className="w3-button w3-theme-d2 w3-margin-bottom"><i className="fa fa-comment" /> &nbsp;Comment</button> 
+         </div> 
+         {/* End Middle Column */}
+       </div>
+       {/* Right Column */}
+       <div className="w3-col m2">
+         <div className="w3-card w3-round w3-white w3-center">
+           <div className="w3-container">
+             <p>Upcoming Events:</p>
+             <img src="/w3images/forest.jpg" alt="Forest" style={{width: '100%'}} />
+             <p><strong>Holiday</strong></p>
+             <p>Friday 15:00</p>
+             <p><button className="w3-button w3-block w3-theme-l4">Info</button></p>
+           </div>
+         </div>
+         <br />
+         <div className="w3-card w3-round w3-white w3-center">
+           <div className="w3-container">
+             <p>Friend Request</p>
+             <img src="/w3images/avatar6.png" alt="Avatar" style={{width: '50%'}} /><br />
+             <span>Jane Doe</span>
+             <div className="w3-row w3-opacity">
+               <div className="w3-half">
+                 <button className="w3-button w3-block w3-green w3-section" title="Accept"><i className="fa fa-check" /></button>
+               </div>
+               <div className="w3-half">
+                 <button className="w3-button w3-block w3-red w3-section" title="Decline"><i className="fa fa-remove" /></button>
+               </div>
+             </div>
+           </div>
+         </div>
+         <br />
+         <div className="w3-card w3-round w3-white w3-padding-16 w3-center">
+           <p>ADS</p>
+         </div>
+         <br />
+         <div className="w3-card w3-round w3-white w3-padding-32 w3-center">
+           <p><i className="fa fa-bug w3-xxlarge" /></p>
+         </div>
+         {/* End Right Column */}
+       </div>
+       {/* End Grid */}
+     </div>
+     {/* End Page Container */}
+   </div>
+   <br />
+   {/* Footer */}
+   <footer className="w3-container w3-theme-d3 w3-padding-16">
+     <h5>Footer</h5>
+   </footer>
+   <footer className="w3-container w3-theme-d5">
+     <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
+   </footer>
+ </div>
     )
 }
 
