314b5eb447b1df3d8d1339bd86830035bd07f1d6
gaith1990 <gaith.mhathbi2@gmail.com>
2021-01-06 23:03:06 +0100

    calendrier final

--- client/src/Components/calendrie/calendrie.js
+++ client/src/Components/calendrie/calendrie.js
@@ -1,49 +1,35 @@
 import React, {useEffect, useState} from 'react';
 import 'react-modern-calendar-datepicker/lib/DatePicker.css';
 import {Calendar} from 'react-modern-calendar-datepicker';
-import {
-  editCalendrier,
-  editProfile,
-  getprofilebyid,
-} from '../../JS/actions/profile';
+import {editCalendrier, getprofilebyid} from '../../JS/actions/profile';
 import {useDispatch, useSelector} from 'react-redux';
-// import {useHistory} from 'react-router-dom';
 
-const Calendrie = () => {
-  const profile = useSelector((state) => state.profileReducer.profile);
-
+const Calendrie = ({calendrier}) => {
+  const profile = useSelector((state) => state.profileReducer);
+  const user = useSelector((state) => state.userReducer.user);
   const dispatch = useDispatch();
-  //   const history = useHistory();
-  // ✅ a change in default state: []
-  //   useEffect(() => {
-  //     getprofilebyid(profile._id);
-  //   }, []);
-  console.log(profile);
-  const calendrier = useSelector(
-    (state) => state.profileReducer.profile.calendrie
-  );
-
-  //   const calend = Array.from(calendrier);
-
-  //   console.log({calendrier: calendrier.forEach((el) => el.shift())});
-  const [selectedDays, setSelectedDays] = useState(calendrier);
-  //   profile.calendrie = selectedDays;
-  //   console.log(profile.calendrie);
-
+  const [selectedDays, setSelectedDays] = useState([]);
+  useEffect(() => {
+    getprofilebyid(profile._id);
+    setSelectedDays(calendrier);
+  }, [calendrier]);
   return (
     <div>
       <Calendar
+        defaultvalue={calendrier}
         value={selectedDays}
         onChange={setSelectedDays}
         shouldHighlightWeekends
       />
-      <button
-        onClick={() => {
-          dispatch(editCalendrier(profile._id, selectedDays));
-        }}
-      >
-        save calendrier
-      </button>
+      {user._id === profile.profile.userId ? (
+        <button
+          onClick={() => {
+            dispatch(editCalendrier(profile.profile._id, selectedDays));
+          }}
+        >
+          save calendrier
+        </button>
+      ) : null}
     </div>
   );
 };
